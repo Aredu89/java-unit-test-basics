adb8a3685738a6c7d9121ec52ae689bd3810be37
Aredu89 <aredu_r89@hotmail.com>
2026-01-29 17:28:14 -0600

    Assert equals, not equals, nulls and not nulls

---
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -0,0 +1,27 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoUtilsTest {
+
+    @Test
+    void testEqualsAndNotEquals() {
+        DemoUtils demoUtils = new DemoUtils();
+
+        assertEquals(6, demoUtils.add(2, 4), "2 + 4 must equal 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not equal 6");
+    }
+
+    @Test
+    void testNullAndNotNull() {
+        DemoUtils demoUtils = new DemoUtils();
+
+        String str1 = null;
+        String str2 = "Luv2Code";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+    }
+}
