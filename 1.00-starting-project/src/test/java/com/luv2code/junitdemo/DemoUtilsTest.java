0c52a736cd5d09d9a33d5d5424ce18fa94b6b142
Aredu89 <aredu_r89@hotmail.com>
2026-02-12 17:26:17 -0600

    Testing unit test lifecycle

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,14 +1,22 @@
 package com.luv2code.junitdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void beforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("Before Each executed test");
+    };
 
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testEqualsAndNotEquals");
 
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 must equal 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not equal 6");
@@ -16,7 +24,7 @@ class DemoUtilsTest {
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testNullAndNotNull");
 
         String str1 = null;
         String str2 = "Luv2Code";
