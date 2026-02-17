081eb68da88e4578dc88f4300910328b6e28cae6
Aredu89 <aredu_r89@hotmail.com>
2026-02-17 15:31:38 -0600

    Refactor TDD code

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -2,13 +2,26 @@ package com.luv2code.tdd;
 
 public class FizzBuzz {
     public String evaluate(int num) {
-        if (num % 3 == 0 && num % 5 == 0) {
-            return "FizzBuzz";
-        } else if (num % 3 == 0) {
-            return "Fizz";
-        } else if (num % 5 == 0) {
-            return "Buzz";
+        StringBuilder result = new StringBuilder();
+        if(num % 3 == 0) {
+            result.append("Fizz");
         }
-        return String.valueOf(num);
+        if(num % 5 == 0) {
+            result.append("Buzz");
+        }
+        if(result.isEmpty()) {
+            result.append(num);
+        }
+        return result.toString();
     }
+//    public String evaluate(int num) {
+//        if (num % 3 == 0 && num % 5 == 0) {
+//            return "FizzBuzz";
+//        } else if (num % 3 == 0) {
+//            return "Fizz";
+//        } else if (num % 5 == 0) {
+//            return "Buzz";
+//        }
+//        return String.valueOf(num);
+//    }
 }
